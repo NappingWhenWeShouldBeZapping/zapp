13a9cd913c79c63ca8dd5760effb847d324091bf
Sam <samkele@bbd.co.za>
2025-03-23 20:47:32 +0200

    added scanning

--- cli/src/main/java/com/scaffoldcli/zapp/ZappApplication.java
+++ cli/src/main/java/com/scaffoldcli/zapp/ZappApplication.java
@@ -3,10 +3,12 @@ package com.scaffoldcli.zapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.shell.command.annotation.CommandScan;
 
 @SpringBootApplication
 @CommandScan
+@ComponentScan
 public class ZappApplication {
 
 	public static void main(String[] args) throws Exception {
