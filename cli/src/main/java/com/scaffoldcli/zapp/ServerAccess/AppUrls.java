2bc3cba7e0f1157a53cdc9aca3f4bde4988113d2
MnqobiNkabinde <mnqobi.nkabinde@bbd.co.za>
2025-03-23 14:23:16 +0200

    Changed auth on the server side

--- cli/src/main/java/com/scaffoldcli/zapp/ServerAccess/AppUrls.java
+++ cli/src/main/java/com/scaffoldcli/zapp/ServerAccess/AppUrls.java
@@ -2,7 +2,7 @@ package com.scaffoldcli.zapp.ServerAccess;
 
 public class AppUrls {
     private static final String client = "http://localhost:8001/";
-    private static final String server="http://13.245.75.144:8080";
+    private static final String server="http://localhost:8002/";
     private static final String googleTokenInfo = "https://oauth2.googleapis.com/tokeninfo?access_token=";
 
     public static String getClient(){
